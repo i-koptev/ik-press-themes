0baeb90955b878af7d72f76acaec394a1b02d36c
Igor Koptev <igor.koptef@gmail.com>
2020-08-28 12:11:51 +0300

    Post template to view stuff move

--- sites/site-ik-press/src/templates/Post/Post.js
+++ sites/site-ik-press/src/templates/Post/Post.js
@@ -1,110 +1,12 @@
 import React from "react"
-import { Link, graphql } from "gatsby"
-
-import { makeStyles, useTheme, createMuiTheme } from "@material-ui/core/styles"
-import Grid from "@material-ui/core/Grid"
-import Container from "@material-ui/core/Container"
-import Typography from "@material-ui/core/Typography"
-import Button from "@material-ui/core/Button"
-
 import MainLayout from "../../layouts/Main"
-import Menu from "../../components/Menu"
+import PostView from "../../views/PostView"
 
-const useStyles = makeStyles(theme => ({
-    header: {
-        marginTop: "1rem",
-        marginBottom: "1rem",
-    },
-    buttonLink: {
-        textDecoration: "none",
-    },
-    linkButton: {
-        [theme.breakpoints.up("md")]: {
-            backgroundColor: "maroon",
-            "&:hover": {
-                backgroundColor: "red",
-            },
-        },
-        // padding: "0.5em 1em 0.5em 2em",
-        // fontSize: "0.7rem",
-        border: `1px solid ${theme.mainNavigationLinkColor}`,
-        marginTop: "1rem",
-        backgroundColor: "tomato",
-        "&:hover": {
-            backgroundColor: "red",
-        },
-    },
-    htmlContent: {
-        "& h2": {
-            ...theme.typography.h5,
-            // paddingBottom: `${theme.siteSpacing.aboutPage * 4}px`,
-            // paddingLeft: `${theme.siteSpacing.aboutPage * 8}px`,
-            fontFamily: "PT Sans Narrow",
-            fontWeight: 400,
-            color: "#fffc",
-            // textAlign: "center",
-        },
-        "& p": {
-            ...theme.html.paragraph,
-            textIndent: "3%",
-        },
-    },
-    imageWrapper: {
-        width: "50%",
-        float: "left",
-        marginRight: "3vw",
-        marginBottom: "1vw",
-        // border: "1px solid #fff3",
-        padding: "0.7vw",
-        paddingBottom: "2vw",
-        // backgroundColor: "#fff1",
-    },
-}))
-
-const Post = ({ pageContext, data, location }) => {
-    const classes = useStyles()
-    const theme = useTheme()
-
+const Post = props => {
+    const { pageContext } = props
     return (
         <MainLayout lang={pageContext.lang}>
-            <Container maxWidth={theme.siteContainer.maxWidth}>
-                <Typography
-                    className={classes.header}
-                    variant="h3"
-                    component="h1"
-                    align="center">
-                    {data.thisPost.title}
-                </Typography>
-
-                <div
-                    style={{
-                        fontSize: "14px",
-                        backgroundColor: "#345",
-                        color: "#eee",
-                        padding: "1rem",
-                    }}>
-                    <pre>
-                        <b>Template:</b> {__filename}
-                    </pre>
-                    <pre>
-                        <b>Page location:</b>
-                        <br />
-                        {JSON.stringify(location, null, 2)}
-                    </pre>
-                    <pre>
-                        <b>Page context:</b>
-                        <br />
-                        {JSON.stringify(pageContext, null, 2)}
-                    </pre>
-                    <pre>
-                        <b>Page data:</b>
-                        <br />
-                        {JSON.stringify(data, null, 2)}
-                    </pre>
-                </div>
-
-                <Menu lang={pageContext.lang} />
-            </Container>
+            <PostView {...props} />
         </MainLayout>
     )
 }
--- sites/site-ik-press/src/views/PostView/PostView.js
+++ sites/site-ik-press/src/views/PostView/PostView.js
@@ -1,25 +1,187 @@
 import React from "react"
+import clsx from "clsx"
+import PropTypes from "prop-types"
+
+import { useStaticQuery, graphql } from "gatsby"
+
 import { makeStyles } from "@material-ui/styles"
 import { Grid } from "@material-ui/core"
-
-// import
+import Typography from "@material-ui/core/Typography"
 
 const useStyles = makeStyles(theme => ({
     root: {
         padding: theme.spacing(4),
+        [theme.breakpoints.up("lg")]: {
+            // backgroundColor: "tomato",
+        },
+    },
+    header: {
+        padding: "1rem",
+    },
+    htmlContent: {
+        "& h1": {
+            ...theme.typography.h3,
+            // paddingBottom: `${theme.siteSpacing.aboutPage * 4}px`,
+            paddingLeft: `${theme.siteSpacing.aboutPage * 8}px`,
+            // fontFamily: "PT Sans Narrow",
+            // fontWeight: 400,
+            // color: "#fffc",
+            // color: "#00f",
+            // textAlign: "center",
+            // textShadow:
+            //     "3px 3px 10px rgba(255,0,0,0.5), -3px -3px 10px rgba(0,0,255,0.5)",
+        },
+        "& p": {
+            ...theme.html.paragraph,
+        },
+        "& .alignleft": {
+            float: "left",
+            margin: "1rem",
+            marginRight: "2.5rem",
+            marginLeft: 0,
+            display: "flow-root",
+        },
+        "& .alignleft:after": {
+            content: ".",
+            display: "block",
+            height: 0,
+            clear: "both",
+            visibility: "hidden",
+        },
+        "& .gatsby-image-wrapper ": {
+            border: "10px solid #fff",
+            boxShadow: "3px 3px 10px 3px rgba(0,0,0,0.2)",
+            // width: "30% !important",
+            // boxShadow:
+            //     " 3px 3px 10px 2px rgba(255,0,0,0.5), -3px -3px 10px 2px rgba(0,0,255,0.5)",
+        },
     },
 }))
 
-const PostView = ({ pageContext }) => {
+const PostView = props => {
+    const { pageContext, className, ...rest } = props
+
     const classes = useStyles()
 
+    const {
+        data: {
+            thisPost: {
+                slug: thisPostSlug,
+                uri: thisPostUri,
+                title: thisPostTitle,
+                content: thisPostContent,
+            },
+        },
+    } = props.data.thisPost ? props : null
+
+    const {
+        data: {
+            nextPost: {
+                slug: nextPostSlug,
+                uri: nextPostUri,
+                title: nextPostTitle,
+                content: nextPostContent,
+            },
+        },
+    } = props.data.nextPost
+        ? props
+        : {
+              data: {
+                  nextPost: {
+                      slug: null,
+                      uri: null,
+                      title: null,
+                      content: null,
+                  },
+              },
+          }
+    const {
+        data: {
+            previousPost: {
+                slug: previousPostSlug,
+                uri: previousPostUri,
+                title: previousPostTitle,
+                content: previousPostContent,
+            },
+        },
+    } = props.data.previousPost
+        ? props
+        : {
+              data: {
+                  previousPost: {
+                      slug: null,
+                      uri: null,
+                      title: null,
+                      content: null,
+                  },
+              },
+          }
+
     return (
-        <Grid container spacing={4}>
+        <Grid
+            container
+            spacing={4}
+            {...rest}
+            className={clsx(classes.root, className)}>
             <Grid item xs={12}>
-                wer
+                <pre>{JSON.stringify(props.data, null, 4)}</pre>
+                <Typography
+                    className={classes.header}
+                    variant="h3"
+                    component="h1"
+                    align="center">
+                    {thisPostTitle}
+                </Typography>
+                <Typography
+                    className={classes.htmlContent}
+                    component="div"
+                    dangerouslySetInnerHTML={{
+                        __html: thisPostContent,
+                    }}
+                />
+            </Grid>
+            <Grid item xs={12}>
+                <DebugDataContext {...props} />
             </Grid>
         </Grid>
     )
 }
 
+PostView.propTypes = {
+    className: PropTypes.string,
+}
+
 export default PostView
+
+export const DebugDataContext = props => {
+    const { pageContext = null, data = null } = props
+
+    return (
+        <div
+            style={{
+                fontSize: "16px",
+                backgroundColor: "#345",
+                color: "#eee",
+                padding: "1rem",
+                display: "block",
+            }}>
+            <pre>
+                <b>Template:</b> {__filename}
+            </pre>
+            <pre>
+                <b>Page context:</b>
+                <br />
+                {JSON.stringify(pageContext, null, 2)}
+            </pre>
+            <pre>
+                <b>Page data:</b>
+                <br />
+                {JSON.stringify(data, null, 2)}
+            </pre>
+            <pre>
+                <b>All Props:</b>
+                {JSON.stringify(props, null, 2)}
+            </pre>
+        </div>
+    )
+}
